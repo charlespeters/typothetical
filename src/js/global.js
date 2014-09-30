6c5a0fd476b49d09bbcbb1caf8655a714a78560a
charlespeters <charlespeters42@gmail.com>
2014-09-30 14:01:46 -0400

    updating the form, adding icons

--- src/js/global.js
+++ src/js/global.js
@@ -1,12 +1,3 @@
-// $(document).ready(function() {
-//     $('.type--controls_family select').change(function() {
-//         $("#typothetical--article")
-//             .addClass( $(this).attr('value') );
-//             // .attr( 'class', $(this).attr('value') );
-//     });
-// });
-
-
 $( ".type--controls_family select" )
   .change(function () {
     var str = "";
